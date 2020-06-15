753b30d8db4e60f554e3cf693682a480eaae22a2
dblancoformacion <dblancoformacion@gmail.com>
2020-06-15 10:52:54 +0200

    Control del flujo de ejecución y quito los x5 de las tablas

--- paso1.py
+++ paso1.py
@@ -1,4 +1,12 @@
+print('hola')
+for i in range(0,3):
+	print(2)
+
+
+'''
 for j in range(1,11):
 	print('Tabla del ',j)
 	for i in range(1,11):
-		print(j,' x ',i,' = ',j*i)
+		if i!=5:
+			print(j,' x ',i,' = ',j*i)
+'''
