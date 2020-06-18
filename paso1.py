973ebf94fa5e0cd8be141ffcbcc73b49bef495db
dblancoformacion <dblancoformacion@gmail.com>
2020-06-18 11:08:35 +0200

    Desglose IVA

--- paso1.py
+++ paso1.py
@@ -1,8 +1,27 @@
 # para el próximo día: encapsulamos código
+def desglose(total,tipo_iva):
+	bi=round(total/(1+tipo_iva/100),2)
+	print(bi)
+	print('+',round(bi*tipo_iva/100,2),sep='')
+	print('-----')
+	print(round(bi*(1+tipo_iva/100),2))
 
-print('hola')
-for i in range(0,3):
-	print(2)
+
+desglose(100,7)
+
+'''
+100
++21
+---
+121
+'''
+
+
+'''
+n=3
+for i in range(n):
+	print(i)
+'''
 
 
 '''
