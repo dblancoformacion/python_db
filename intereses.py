4bcd2c76a362e1017af205d71ee1a53a0701c058
dblancoformacion <dblancoformacion@gmail.com>
2020-06-26 10:34:02 +0200

    Sistema francés

--- intereses.py
+++ intereses.py
@@ -1,7 +1,21 @@
 # Calculadora de intereses
 
-n_cuotas=24 #mensuales
-capital=1000
-tipo=5
+def cuota(capital,tipo,meses):
+	interes=(1+tipo/100)**(1/12)-1 # 12 por ser tipo de interés anual
+	interes*=100
+	return round(
+		capital*interes
+		/
+		(
+			100*(1-(1+interes/100)**(-meses))
+		)
+		,2)
+	
 
-cuota = n_cuotas
+capital=1000
+meses=12
+tipo=10
+
+pago=cuota(capital,tipo,meses);
+print('Cuota mensual : ',pago)
+print('Intereses abonados en total: ',round(pago*meses-capital,2))
