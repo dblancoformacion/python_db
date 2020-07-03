12a76765a9438d4a48a420a5dfa7086f74af64ad
dblancoformacion <dblancoformacion@gmail.com>
2020-07-03 10:59:37 +0200

    Conversión a funciones

--- paso4.py
+++ paso4.py
@@ -1,18 +1,21 @@
-﻿# prima de emisión
+﻿def calculo_prima_emision(n_acciones,v_nominal,desembolso_accionistas):
+	"""
+	n_acciones,
+	v_nominal,
+	desembolso_accionistas
+	"""
+	prima_emision_total = (
+		desembolso_accionistas - n_acciones*v_nominal
+	)
+	prima_emision=prima_emision_total/n_acciones
+	'''
+	print(v_nominal,'Precio de la acción')
+	print(prima_emision_total,'Prima de emisión total')
+	print(prima_emision,'Prima de emisión')
+	print(v_nominal+prima_emision,'Precio emisión')
+	'''
+	return v_nominal+prima_emision
+	
 
-n_acciones = 30000 #acciones
-v_nominal = 5 # valor nominal por acción
-desembolso_accionistas = 240000
-
-# prima de emisión total
-
-prima_emision_total = (
-	desembolso_accionistas - n_acciones*v_nominal
-)
-prima_emision=prima_emision_total/n_acciones
-
-print(v_nominal,'Precio de la acción')
-print(prima_emision_total,'Prima de emisión total')
-print(prima_emision,'Prima de emisión')
-print(v_nominal+prima_emision,'Precio emisión')
-
+print(calculo_prima_emision(30000,5,240000))
+print(calculo_prima_emision(50000,5,300000))
